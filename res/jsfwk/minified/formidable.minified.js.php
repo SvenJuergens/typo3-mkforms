c6b071c66266a8c4c99d97e2bc4f6a4cee12e76a
Pavel Klinkov <pavel.klinkov@dmk-ebusiness.de>
2014-10-24 14:58:59 +0200

    Revert Res

--- res/jsfwk/minified/formidable.minified.js.php
+++ res/jsfwk/minified/formidable.minified.js.php
@@ -1,27 +1,4 @@
 <?php
-/***************************************************************
- *  Copyright notice
- *
- *  (c) 2009-2014 DMK E-BUSINESS GmbH (dev@dmk-ebusiness.de)
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
-
 	header("Content-Type: text/javascript");
 	header("Content-Encoding: gzip");
 	header("Cache-Control: max-age=86400, public");
--- res/minify/minify_formidable.php
+++ res/minify/minify_formidable.php
@@ -1,27 +1,4 @@
 <?php
-/***************************************************************
- *  Copyright notice
- *
- *  (c) 2009-2014 DMK E-BUSINESS GmbH (dev@dmk-ebusiness.de)
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
-
 
 die("DISABLED");
 die('<h1>DONT USE THIS!</h1><p>Durch die verwendung verschiedener JavaScript Framework über den MKWrapper (jQuery, Prototype), wurde die Minimierung von Scripten umgestellt!<br /> Das minimierte Script muss sich nun im selben Verzeichniss befinden und .min behinhalten <br /> framework.js - framework.min.js</p>');
